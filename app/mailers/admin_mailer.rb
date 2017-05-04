88f3f0456b5e153fc267f034495de882964793b5
Natanael H. Aguiar <natanaelhaguiar@gmail.com>
2017-05-04 01:42:25 +0000

    message mailer adicionado na index

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,7 +1,15 @@
 class AdminMailer < ApplicationMailer
     def update_email(current_admin, admin)
-        @current_admin = current_admin
-        @admin = admin
-        mail(to: @admin.name, subject: "Seus dados foram alterados")
+      @current_admin = current_admin
+      @admin = admin
+      mail(to: @admin.name, subject: "Seus dados foram alterados")
+    end
+
+    def send_message(current_admin, recipient, subject, message)
+      @current_admin = current_admin
+      @recipient = recipient
+      @subject = subject
+      @message = message
+      mail(to: @recipient, subject: @subject)
     end
 end
